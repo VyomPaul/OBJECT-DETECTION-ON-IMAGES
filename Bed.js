a9dfa509f187c9f7698b2d912b205e2c56c5a766
VyomPaul <86235294+VyomPaul@users.noreply.github.com>
2022-03-22 18:26:15 +0530

    Add files via upload

--- Bed.js
+++ Bed.js
@@ -1,4 +1,5 @@
 var status = "";
+results = [];
 
 function preload() {
     image = loadImage('BED.jpg');
@@ -22,3 +23,14 @@ function gotResult() {
     }
     console.log(results);
 }
+
+function draw() {
+    if(status != "") 
+    {
+        for(var i = 0; i < objects.length; i++) {
+            text(objects[i].label + " " + percent +"%", objects[i].x, objects[i].y);
+            percent = floor(objects[i].confidence * 100);
+        }
+    }
+}
+
